68ac5d03b0192a8d199c0c122b7fbe882c2ccbb8
Victor Michnowicz <vmichnowicz@yahoo.com>
2011-10-30 17:04:45 -0400

    Add new model cuz admin page fails without it

---
+++ models/poll_m.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * Poll model
+ *
+ * @author Victor Michnowicz
+ * @category Modules
+ */
+class Poll_m extends MY_Model {}
