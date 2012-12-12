356a397ad974ffc12d4a1874102b0fb3ec59a863
Victor Michnowicz <vmichnowicz@yahoo.com>
2012-12-12 18:06:24 -0500

    Update Portuguese & add Spanish

--- language/brazilian/polls_lang.php
+++ language/brazilian/polls_lang.php
@@ -1,87 +1,88 @@
 <?php
 
+
 /**
- * Brazilian language file
+ * Brazilian Portuguese language file
  *
- * @author Leonardo Caitano
- * @link https://leonardocaitano.com.br/
+ * @author Paulino Pardo
+ * @link http://www.webbin-design.com
  */
 
 // What it be?
-$lang['polls.polls']	 				= 'Enquetes';
+$lang['polls.polls']                    = 'Enquetes';
 
 // Shortcut links
-$lang['polls.list_label'] 				= 'Lista de Enquetes';
-$lang['polls.new_poll_label']			= 'Criar nova Enquete';
+$lang['polls.list_label']               = 'Lista Enquetes';
+$lang['polls.new_poll_label']           = 'Criar uma nova enquete';
 
 // Success notifications
-$lang['polls.create_success'] 			= 'Enquete criada com sucesso.';
-$lang['polls.update_success'] 			= 'A enquete foi atualizada com sucesso.';
-$lang['polls.delete_success']			= 'Todas as enquetes foram excluidas com sucesso.';
-$lang['polls.install_success']			= 'O módulo de Enquetes foi instalado com sucesso.';
+$lang['polls.create_success']           = 'Nova enquete criada com sucesso.';
+$lang['polls.update_success']           = 'A enquete foi atualizada com sucesso.';
+$lang['polls.delete_success']           = 'Todas as enquetes foram deletadas com sucesso.';
+$lang['polls.install_success']          = 'O m&oacute;dulo de enquetes foi instalado com sucesso.';
 
 // Error notifications
-$lang['polls.create_error']				= 'A enquete não pôde ser criada.';
-$lang['polls.not_exist_error']			= 'Esta enquete não existe.';
-$lang['polls.invalid_date']				= 'A data fornecida é inválida.';
-$lang['polls.exists_error']				= 'A enquete especificada não existe.';
-$lang['polls.update_error']				= 'A enquete não pôde ser atualizada.';
-$lang['polls.delete_error']				= 'Esta enquete não pôde ser exlcuida.';
-$lang['polls.id_error']					= 'Nenhum ID foi especificado.';
-$lang['polls.no_poll_options_error']	= 'Não existe nenhuma opção para esta enquete.';
-$lang['polls.no_polls_error'] 			= 'Nenhuma enquete foi criada até o momento.';
-$lang['polls.install_error']			= 'O módulo não pode ser instalado.';
-$lang['polls.cookies_required'] 		= 'This poll requires cookies to be enabled in order to vote.';#translate
-$lang['polls.no_options_submitted'] 	= 'No poll options submitted.';#translate
+$lang['polls.create_error']             = 'N&atilde;o foi poss&iacute;vel criar a enquete.';
+$lang['polls.not_exist_error']          = 'Essa enquete n&atilde;o existe.';
+$lang['polls.invalid_date']             = 'Data n&atilde;o v&aacute;lida';
+$lang['polls.exists_error']             = 'A enquete solicitada n&atilde;o existe.';
+$lang['polls.update_error']             = 'N&atilde;o foi poss&iacute;vel atualizar a enquete.';
+$lang['polls.delete_error']             = 'Essa enquete n&atilde;o pode ser deletada.';
+$lang['polls.id_error']                 = 'O ID n&atilde;o foi especificado.';
+$lang['polls.no_poll_options_error']    = 'Essa enquete n&atilde;o tem op&ccedil;&otilde;es';
+$lang['polls.no_polls_error']           = 'Ainda n&atilde;o foram criadas enquetes.';
+$lang['polls.install_error']            = 'O m&oacute;dulo n&atilde;o pode ser instalado.';
+$lang['polls.cookies_required']         = 'Essa enquete precissa ter as cookies habilitadas para poder votar.';
+$lang['polls.no_options_submitted']     = 'As opciones da enquete n&atilde;oforam salvas.';
 
 // Polls overview headers
-$lang['polls.title']					= 'Titulo';
-$lang['polls.active']					= 'Ativa';
-$lang['polls.num_options']				= 'Opções';
-$lang['polls.open_date']				= 'Abertura';
-$lang['polls.close_date']				= 'Encerramento';
-$lang['polls.created']					= 'Criada em';
-$lang['polls.last_updated']				= 'Atualizada em';
+$lang['polls.title']                    = 'Titulo da enquete';
+$lang['polls.active']                   = 'Ativa';
+$lang['polls.num_options']              = 'Op&ccedil;&otilde;es';
+$lang['polls.open_date']                = 'Data de inicio';
+$lang['polls.close_date']               = 'Data de fim';
+$lang['polls.created']                  = 'Criada';
+$lang['polls.last_updated']             = '&Uacute;ltima modifica&ccedil;&atilde;o';
 
 // Polls overview actions
-$lang['polls.view_label']				= 'Visualizar Enquete';
-$lang['polls.update_label']				= 'Atualizar Enquete';
-$lang['polls.results_label']			= 'Resultados da Enquete';
-$lang['polls.delete_label']				= 'Excluir Enquete';
+$lang['polls.view_label']               = 'Ver Enquete';
+$lang['polls.update_label']             = 'Atualizar Enquete';
+$lang['polls.results_label']            = 'Resultados da enquete';
+$lang['polls.delete_label']             = 'Deletar enquete';
 
 // Create a new poll labels
-$lang['polls.title_label']				= 'Titulo';
-$lang['polls.slug_label']				= 'Nome Curto';
-$lang['polls.type_label']				= 'Tipo';
-$lang['polls.description_label']		= 'Descrição da Enquete';
-$lang['polls.options_label']			= 'Opções da Enquete';
-$lang['polls.add_option_label']			= 'Adicionar Opção';
-$lang['polls.multiple_votes_label']		= 'Múltiplos Votos';
-$lang['polls.open_date_label']			= 'Data de Abertura (YYYY-MM-DD)';
-$lang['polls.close_date_label']			= 'Data de Encerramento (YYYY-MM-DD)';
-$lang['polls.comments_label']			= 'Habilitar Comentários';
-$lang['polls.members_only_label']		= 'somente Membros';
-$lang['polls.active_label']				= 'Ativa';
+$lang['polls.title_label']              = 'Titulo';
+$lang['polls.slug_label']               = 'Slug';
+$lang['polls.type_label']               = 'Tipo';
+$lang['polls.description_label']        = 'Descrip&ccedil;&atilde;o da enquete';
+$lang['polls.options_label']            = 'Op&ccedil;&otilde;es da enquete';
+$lang['polls.add_option_label']         = 'Adicionar Op&ccedil;&atilde;o';
+$lang['polls.multiple_votes_label']     = 'Votos Multiples';
+$lang['polls.open_date_label']          = 'Data de inicio (YYYY-MM-DD)';
+$lang['polls.close_date_label']         = 'Data de fim (YYYY-MM-DD)';
+$lang['polls.comments_label']           = 'Habilitar Comentarios';
+$lang['polls.members_only_label']       = 'S&oacute; para Membros';
+$lang['polls.active_label']             = 'Ativa';
 
 // Poll creation and modification info
-$lang['polls.options_info'] 			= 'A poll option can either be <strong>defined</strong> or <strong>other</strong>. A <strong>defined</strong> poll option lets the user vote for a poll option that you specify. An <strong>other</strong> poll options allows the user to enter text into a text input for that particular option.';#translate
-$lang['polls.type_info'] 				= 'A poll can either be <strong>single</strong> or <strong>multiple</strong>. A <strong>single</strong> poll restricts users to only voting for one poll option. A <strong>multiple</strong> poll allows the user to vote for multiple poll options.';#translate
+$lang['polls.options_info']             = 'As op&ccedil;&otilde;es das enquetes podem ser de tipo <strong>definida</strong> ou <strong>outra</strong>. A op&ccedil;&atilde;o de enquete <strong>definida</strong>  permite ao usuario votar pela op&ccedil;&atilde;o que voc&ecirc;  definiu. A op&ccedil;&atilde;o de enquete <strong>outra</strong> permite ao usuario escrever umn texto para essa op&ccedil;&atilde;o.';
+$lang['polls.type_info']                = 'A enquete pode ser de voto <strong>&uacute;nico</strong> ou <strong>multiple</strong>. Nas enquetes de voto <strong>&uacute;nico</strong> o voto do usuario fica restrito a uma s&oacute; op&ccedil;&atilde;o. As enquetes de voto <strong>multiple</strong> permitem ao usuario escolher multiples op&ccedil;&otilde;es de voto.';
 
 // Poll types
-$lang['polls.single']					= 'Único';
-$lang['polls.multiple']					= 'Múltiplo';
+$lang['polls.single']                   = '&Uacute;nico';
+$lang['polls.multiple']                 = 'Multiple';
 
 // Poll option types
-$lang['polls.defined'] 					= 'Definido';
-$lang['polls.other'] 					= 'Outros';
+$lang['polls.defined']                  = 'Definida';
+$lang['polls.other']                    = 'Outra';
 
 // General
-$lang['polls.yes']						= 'Sim';
-$lang['polls.no']						= 'Não';
+$lang['polls.yes']                      = 'Sim';
+$lang['polls.no']                       = 'N&atilde;o';
 
 // View a poll
-$lang['polls.total_votes']				= 'Total de votos';
-$lang['polls.vote']						= 'Voto';
-$lang['polls.votes']					= 'Votos';
-$lang['polls.results']					= 'Resultados';
-$lang['polls.no_options']				= 'Não há opções para esta enquete';
+$lang['polls.total_votes']              = 'Total de votos';
+$lang['polls.vote']                     = 'Voto';
+$lang['polls.votes']                    = 'Votos';
+$lang['polls.results']                  = 'Resultados';
+$lang['polls.no_options']               = 'Essa enquete n&atilde;o tem opc&ccedil;&otilde;es';
